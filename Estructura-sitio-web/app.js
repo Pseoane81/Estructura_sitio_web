d53240db7bbd51bc175b58050819a23d8335817e
Pablo Seoane <pablitoseoane@gmail.com>
2021-09-06 16:06:24 -0300

    Ejercicio base del PG

---
+++ Estructura-sitio-web/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const publicpath = path.resolve(__dirname, "./public")
+//const home = path.resolve(__dirname)
+
+app.use(express.static(publicpath))
+app.listen(3000, () => {
+    console.log("abrimos puerto 3000")
+})
+app.get('/',(req,res) => {
+    res.sendFile(path.resolve(__dirname,"./views/home.html"))
+})
